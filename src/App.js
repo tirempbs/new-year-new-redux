d05da9d972d2694d38d6c0ce31a6b374eb3a666f
hwangjf <jeffreyhwang90@gmail.com>
2019-01-03 18:10:29 -0500

    work on dis

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({text: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="header">Type here:</h1>
+        <input className="input" type='text' onChange={this.handleChange} value={this.state.text} />
+        <h1 className="header">Your input: <br/>{this.state.text}</h1>
       </div>
     );
   }
