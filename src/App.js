41b29bb48e42b1b5d1263d43904ff9df08fdf41b
tirempbs <tirem@fastmail.com>
2019-01-04 16:52:09 -0500

    finished but with everything on app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 import './App.css';
+
+const UPDATE_TEXT = 'UPDATE_TEXT'
+
+function textReducer(state='', action) {
+  switch (action.type) {
+    case UPDATE_TEXT:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    text: state
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    handleInputUpdate: event => dispatch({type: UPDATE_TEXT, payload: event.target.value})
+  }
+}
+
+const store = createStore(textReducer)
 
 const MyInputs = props => (
   <div className="App">
     <h1 className="header">Your input:</h1>
-    <input className="input" type='text' onChange={props.handleChange} value={props.text} />
+    <input 
+      className="input" 
+      type='text' 
+      onChange={props.handleInputUpdate} 
+      value={props.text} />
     <h1 className="header">Your output: <br />{props.text}</h1>
   </div>
 )
 
+const ConnectedMyInputs = connect(mapStateToProps, mapDispatchToProps)(MyInputs);
+
 class App extends Component {
-  state = {
-    text: ''
-  }
-
-  handleChange = (event) => {
-    this.setState({text: event.target.value})
-  }
-
   render() {
     return (
-      <MyInputs handleChange={this.handleChange} text={this.state.text} />
+      <Provider store={store}>
+        <ConnectedMyInputs />
+      </Provider>
     );
   }
 }
